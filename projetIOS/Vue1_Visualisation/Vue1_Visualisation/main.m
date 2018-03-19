9bc944884df37fd72ce26c5ab31ef7ae9327ef99
Unknown <geronimo650@gmail.com>
2018-03-19 13:20:25 +0100

    VueIOS_1

---
+++ projetIOS/Vue1_Visualisation/Vue1_Visualisation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Vue1_Visualisation
+//
+//  Created by Desman Jérôme on 13/03/2018.
+//  Copyright © 2018 Desman Jérôme. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
