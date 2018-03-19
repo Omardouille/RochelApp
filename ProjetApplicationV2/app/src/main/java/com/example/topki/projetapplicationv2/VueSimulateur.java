c229ba26a8cf10491ecbe9f507e642151e7cf37c
Unknown <julienfillon79@gmail.com>
2018-03-19 16:34:27 +0100

    VueSimulateur

--- ProjetApplicationV2/app/src/main/java/com/example/topki/projetapplicationv2/VueSimulateur.java
+++ ProjetApplicationV2/app/src/main/java/com/example/topki/projetapplicationv2/VueSimulateur.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -12,6 +13,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class VueSimulateur extends FragmentActivity implements OnMapReadyCallback {
 
+    private MapView mapView;
     private GoogleMap mMap;
 
     @Override
