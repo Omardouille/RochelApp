1dedc957681b24111cef80e23b016f219de09a2d
Benjamin <simpson.17@hotmail.fr>
2018-03-19 15:36:30 +0100

    Vues android

--- ProjetApplicationV2/app/src/main/java/com/example/topki/projetapplicationv2/MainActivity.java
+++ ProjetApplicationV2/app/src/main/java/com/example/topki/projetapplicationv2/MainActivity.java
@@ -1,24 +1,42 @@
 package com.example.topki.projetapplicationv2;
 
 import android.annotation.SuppressLint;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity{
 
+    Button boutonInfos;
+    Button  boutonPiloter;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             //Création de la vue
-            Button boutonInfos;
-            Button  boutonPiloter;
+
 
             boutonInfos = new Button(this);
             boutonPiloter = new Button(this);
 
-            VueAccueil vueAccueil = new VueAccueil(this,boutonInfos,boutonPiloter);
+            VueAccueil vueAccueil = new VueAccueil(this, boutonInfos, boutonPiloter);
 
             super.onCreate(savedInstanceState);
             setContentView(vueAccueil);
-    }
+
+            boutonPiloter.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(MainActivity.this, VuePilotage.class));
+                }
+            });
+
+            boutonInfos.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(MainActivity.this, VueSimulateur.class));
+                }
+            });
+
+        }
 }
