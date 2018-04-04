515a815cf6d119d19a6c3515ce37a733fb5db614
Benjamin <simpson.17@hotmail.fr>
2018-04-04 21:40:43 +0200

    VuePilotage

--- ProjetApplicationV2/app/src/main/java/com/example/topki/projetapplicationv2/VuePilotage.java
+++ ProjetApplicationV2/app/src/main/java/com/example/topki/projetapplicationv2/VuePilotage.java
@@ -1,18 +1,34 @@
 package com.example.topki.projetapplicationv2;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class VuePilotage extends FragmentActivity implements OnMapReadyCallback {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+import java.io.IOException;
+
+public class VuePilotage extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
 
     private GoogleMap mMap;
+    ArrayList<LatLng> points = new ArrayList<LatLng>();
+    ArrayList<Marker> markers;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +39,6 @@ public class VuePilotage extends FragmentActivity implements OnMapReadyCallback 
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
 
     /**
      * Manipulates the map once available.
@@ -37,13 +52,92 @@ public class VuePilotage extends FragmentActivity implements OnMapReadyCallback 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMapLongClickListener(this);
         // Add a marker in Sydney and move the camera
-        LatLng lr = new LatLng(46.152994, -1.1560821);
-        float zoom = 15.0f;
-        mMap.addMarker(new MarkerOptions().position(lr).title("La Rochelle"));
+        LatLng lr = new LatLng(46.151194, -1.161005);
+        float zoom = 14.2f;
+        //mMap.addMarker(new MarkerOptions().position(lr).title("La Rochelle"));
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        mMap.getUiSettings().setScrollGesturesEnabled(false);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lr,zoom));
+        mMap.getUiSettings().setAllGesturesEnabled(false);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lr, zoom));
+        markers = new ArrayList<>();
     }
+
+    @Override
+    public void onMapClick(LatLng point) {
+        points.add(point);
+        Marker lastMarker = mMap.addMarker(new MarkerOptions().position(point).title("Point"+points.size()));
+        markers.add(lastMarker);
+        if(points.size()>1)
+        {
+            PolylineOptions polyLineOptions = new PolylineOptions();
+            polyLineOptions.width(7 * 1);
+            polyLineOptions.geodesic(true);
+            polyLineOptions.color(Color.RED);
+            polyLineOptions.addAll(points);
+            Polyline polyline = mMap.addPolyline(polyLineOptions);
+            polyline.setGeodesic(true);
+        }
+        try {
+            writeFile();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onMapLongClick(LatLng point) {
+        mMap.clear();
+        markers.get(markers.size()-1).remove();
+        markers.remove(markers.size()-1);
+        points.remove(points.size()-1);
+        for (Marker m:markers) {
+            mMap.addMarker(new MarkerOptions().position(m.getPosition()).title(m.getTitle()));
+        }
+        PolylineOptions polyLineOptions = new PolylineOptions();
+        polyLineOptions.width(7 * 1);
+        polyLineOptions.geodesic(true);
+        polyLineOptions.color(Color.RED);
+        polyLineOptions.addAll(points);
+        Polyline polyline = mMap.addPolyline(polyLineOptions);
+        polyline.setGeodesic(true);
+    }
+
+    private void writeFile() throws IOException
+    {
+        JSONObject obj = new JSONObject();
+        for (int i=0;i<points.size();i++)
+        {
+            LatLng latlng = points.get(i);
+            try {
+                obj.put("Waypoint: ",i);
+                obj.put("Latitude: ", latlng.latitude);
+                obj.put("Longitude: ", latlng.longitude);
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+        FileWriter file = new FileWriter("coord.txt");
+        try{
+            file.write(obj.toString());
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            file.flush();
+            file.close();
+        }
+
+    }
+
+
 }
